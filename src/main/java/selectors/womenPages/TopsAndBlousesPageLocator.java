343aa2ffa1e87b5bc7f47deb7a1299b13674253d
Syed Hussain <syed_s_hus@yahoo.com>
2016-02-16 20:12:12 -0500

    .gitignore and TopsandBlousesPageLocator class added

---
+++ src/main/java/selectors/womenPages/TopsAndBlousesPageLocator.java
@@ -0,0 +1,12 @@
+package selectors.womenPages;
+
+/**
+ * Created by sayem on 2/15/16.
+ */
+public class TopsAndBlousesPageLocator {
+
+    public static final String NOLITA_CAMI = "//*[@class='category-products']/ul/li[1]/div/h2/a";
+    public static final String DELANCY_CARDIGAN = "//*[@class='category-products']/ul/li[3]/div/h2/a";
+    public static final String LUDLOW_OXFORD = "//*[@class='category-products']/ul/li[4]/div/h2/a";
+    public static final String BLACK_NOLITA_CAMI = "//*[@class='category-products']/ul/li[6]/div/h2/a";
+}
